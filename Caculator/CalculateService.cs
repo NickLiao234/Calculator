02ebd0cd37e25477257211654ab8dda68157d501
NickLiao <nick_liao@cmoney.com.tw>
2021-06-22 16:40:54 +0800

    init commit

---
+++ Caculator/CalculateService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caculator
+{
+    public class CalculateService
+    {
+        public string HistoryValue { get; set; } = "0";
+        public string CurrentValue { get; set; } = "0";
+
+        public void AppendNumber(string str)
+        {
+            CurrentValue += str;
+
+        }
+
+        public void Add()
+        {
+            var result = Convert.ToDouble(HistoryValue) + Convert.ToDouble(CurrentValue);
+            HistoryValue = result.ToString();
+            CurrentValue = "0";
+        }
+        public void Sub()
+        {
+            var result = Convert.ToDouble(HistoryValue) - Convert.ToDouble(CurrentValue);
+            HistoryValue = result.ToString();
+            CurrentValue = "0";
+        }
+        public void Multiple()
+        {
+            var result = Convert.ToDouble(HistoryValue) * Convert.ToDouble(CurrentValue);
+            HistoryValue = result.ToString();
+            CurrentValue = "0";
+        }
+        public void Divide()
+        {
+            var result = Convert.ToDouble(HistoryValue) / Convert.ToDouble(CurrentValue);
+            HistoryValue = result.ToString();
+            CurrentValue = "0";
+        }
+        public void Equal()
+        {
+
+        }
+        public void Back()
+        {
+            if (CurrentValue.Length == 1)
+            {
+                CurrentValue = "0";
+            }
+
+            CurrentValue.Remove(CurrentValue.Length - 1);
+        }
+        public void Clear()
+        {
+            HistoryValue = "0";
+            CurrentValue = "0";
+        }
+        public void ClearCurrent()
+        {
+            CurrentValue = "0";
+        }
+    }
+}
---
+++ Caculator/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Caculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CalculateService calculateService;
+        private Dictionary<string, Delegate> MapMethod; 
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string a = "1.";
+            var b = Convert.ToDouble(a);
+            var c = 0 - b;
+            var d = c.ToString();
+
+            calculateService = new CalculateService();
+            MapMethod = new Dictionary<string, Delegate>();
+            for (int i = 0; i < 10; i++)
+            {
+                MapMethod.Add(i.ToString(), new Action<string>(param => calculateService.AppendNumber(param)));
+            }
+            MapMethod.Add("+", new Action<string>(c => calculateService.Add()));
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var o = (Button)sender;
+            var a = o.Content.ToString();
+            MapMethod[a].DynamicInvoke(a);
+        }
+
+    }
+}
