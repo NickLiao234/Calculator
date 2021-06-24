cf4d95bab8e1779189579812f1633297cc653f1c
NickLiao <nick_liao@cmoney.com.tw>
2021-06-24 10:30:56 +0800

    設定DI

--- Caculator/App.xaml.cs
+++ Caculator/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,39 @@ namespace Calculator
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// ServiceProvider
+        /// </summary>
+        private readonly ServiceProvider _serviceProvider;
+
+        /// <summary>
+        /// 初始化app
+        /// </summary>
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        /// <summary>
+        /// DI設定
+        /// </summary>
+        /// <param name="services">service collection</param>
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+        }
+
+        /// <summary>
+        /// 啟動app
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">args</param>
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
