36574ed00db04c1aa1e6c8b6d399d64fa43e9ddb
NickLiao <nick_liao@cmoney.com.tw>
2021-07-01 16:37:58 +0800

    重構CalculateService, 新增CalculateServiceBase, 新增 postfix 實作

---
+++ Calculator.Core/Services/Calculate/CalculateServiceBase.cs
@@ -0,0 +1,116 @@
+﻿using Caculator.Core.Operators;
+using System.Collections.Generic;
+
+namespace Calculator.Core.Service.Calculate
+{
+    public abstract class CalculateServiceBase
+    {
+        /// <summary>
+        /// 字元類別對應表
+        /// </summary>
+        private Dictionary<string, CalculateElementBase> map;
+
+        public CalculateServiceBase()
+        {
+            initMap();
+        }
+
+        /// <summary>
+        /// 初始化對應表
+        /// </summary>
+        private void initMap()
+        {
+            map = new Dictionary<string, CalculateElementBase>();
+            map.Add("+", new AddElement());
+            map.Add("-", new SubElement());
+            map.Add("*", new MultipleElement());
+            map.Add("/", new DivideElement());
+            //map.Add("(", new OpenParentthesisElement());
+            //map.Add(")", new CloseParentthesisElement());
+        }
+
+        /// <summary>
+        /// 取得表達式字串
+        /// </summary>
+        /// <param name="expression">未排序過表達式</param>
+        /// <returns>字串</returns>
+        public abstract string GetExpressionString(List<string> expression);
+
+        /// <summary>
+        /// 取得表達式List
+        /// </summary>
+        /// <param name="expression">未排序過表達式</param>
+        /// <returns>List<CalculateElementBase></returns>
+        public abstract List<CalculateElementBase> GetExpressionList(List<string> expression);
+
+        /// <summary>
+        /// 取得表達式運算結果
+        /// </summary>
+        /// <param name="expression">未排序過表達式</param>
+        /// <returns>decimal</returns>
+        public abstract decimal GetCalculateResult(List<string> expression);
+
+        /// <summary>
+        /// 取得運算子暫存堆疊第一個物件的優先權
+        /// </summary>
+        /// <param name="tempStack"></param>
+        /// <returns></returns>
+        protected virtual int GetStackPriority(Stack<OperatorElement> tempStack)
+        {
+            if (tempStack.Count == 0)
+            {
+                return 0;
+            }
+
+            var element = tempStack.Peek();
+
+            if (element.Value == "(")
+            {
+                return 0;
+            }
+
+            return element.Priority;
+        }
+
+        /// <summary>
+        /// 判斷是否為運算元
+        /// </summary>
+        /// <param name="item">欲判斷物件</param>
+        /// <returns></returns>
+        protected bool IsOperand(CalculateElementBase item)
+        {
+            OperandElement operand = item as OperandElement;
+
+            if (operand is null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 表達式List字串轉為List物件
+        /// </summary>
+        /// <param name="expression">表達式</param>
+        /// <returns></returns>
+        protected List<CalculateElementBase> TransferExpressionToListObject(List<string> expression)
+        {
+            var result = new List<CalculateElementBase>();
+
+            foreach (var element in expression)
+            {
+                if (map.ContainsKey(element))
+                {
+                    result.Add(map[element]);
+                }
+                else
+                {
+                    result.Add(new OperandElement(element));
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Calculator.Core/Services/Calculate/PostfixService.cs
@@ -0,0 +1,136 @@
+﻿using Caculator.Core.Operators;
+using System;
+using System.Collections.Generic;
+
+namespace Calculator.Core.Service.Calculate
+{
+    /// <summary>
+    /// 運算後序表達式服務
+    /// </summary>
+    public class PostfixService : CalculateServiceBase
+    {
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public PostfixService():base()
+        {
+        }
+
+        /// <summary>
+        /// 取得後序表達式字串
+        /// </summary>
+        /// <param name="expression">未排序過表達式</param>
+        /// <returns>字串</returns>
+        public override string GetExpressionString(List<string> expression)
+        {
+            string result = "";
+
+            var listPostfix = GetExpressionList(expression);
+
+            foreach (var item in listPostfix)
+            {
+                result += item.Value;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 取得後序表達式
+        /// </summary>
+        /// <param name="expression">未排序過表達式</param>
+        /// <returns>List<CalculateElementBase></returns>
+        public override List<CalculateElementBase> GetExpressionList(List<string> expression)
+        {
+            var result = new List<CalculateElementBase>();
+
+            var listObject = TransferExpressionToListObject(expression);
+
+            var tempStack = new Stack<OperatorElement>();
+
+            foreach (var item in listObject)
+            {
+                if (IsOperand(item))
+                {
+                    result.Add(item);
+                    continue;
+                }
+
+                if (item.Value == ")")
+                {
+                    do
+                    {
+                        var temp = tempStack.Pop();
+
+                        if (temp.Value == "(")
+                        {
+                            break;
+                        }
+
+                        result.Add(temp);
+
+                    } while (true);
+                }
+                else
+                {
+                    if (((OperatorElement)item).Priority > GetStackPriority(tempStack))
+                    {
+                        var element = (OperatorElement)item;
+                        tempStack.Push(element);
+                    }
+                    else
+                    {
+                        while (((OperatorElement)item).Priority <= GetStackPriority(tempStack))
+                        {
+                            var temp = tempStack.Pop();
+                            result.Add(temp);
+                        }
+
+                        var element = (OperatorElement)item;
+                        tempStack.Push(element);
+                    }
+                }
+            }
+
+            while (tempStack.Count != 0)
+            {
+                var temp = tempStack.Pop();
+                result.Add(temp);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 取得後序表達式運算結果
+        /// </summary>
+        /// <param name="expression">未排序過表達式</param>
+        /// <returns>decimal</returns>
+        public override decimal GetCalculateResult(List<string> expression)
+        {
+            var postfixList = GetExpressionList(expression);
+
+            var tempOperandStack = new Stack<decimal>();
+
+            foreach (var element in postfixList)
+            {
+                if (IsOperand(element))
+                {
+                    var value = Convert.ToDecimal(element.Value);
+                    tempOperandStack.Push(value);
+                }
+                else
+                {
+                    var op2 = tempOperandStack.Pop();
+                    var op1 = tempOperandStack.Pop();
+                    var thisOperator = (OperatorElement)element;
+                    var result = thisOperator.Calculate(op1, op2);
+                    tempOperandStack.Push(result);
+                }
+            }
+
+            return tempOperandStack.Pop();
+        }
+    }
+}
